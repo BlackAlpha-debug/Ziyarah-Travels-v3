a237ef5ba500255e4001eeaf02db4c04fd67fb0f
BlackAlpha-debug <mj.muzammiljawad@gmail.com>
2025-09-16 18:30:58 +0500

    visitinng  search

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,5 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Analytics } from '@vercel/analytics/react';
+// Render the app
+createRoot(document.getElementById('root')!).render(
+  <>
+    <App />
+    <Analytics /> {/* ✅ Add Analytics here — after App */}
+  </>
+)
 
 createRoot(document.getElementById("root")!).render(<App />);
